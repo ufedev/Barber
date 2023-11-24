4f77865c7db4cc2f866cf5e601a1d257a87b24c3
Federico Malfasi <Federico.malfasi@gmail.com>
2023-11-23 21:08:48 -0300

    Finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,37 @@
 import Formulario from './components/Formulario'
+import Cita from "./components/Cita"
 import { useState, useEffect } from 'react'
 import { ToastContainer } from "react-toastify"
+import { MagicMotion } from "react-magic-motion"
 import 'react-toastify/dist/ReactToastify.css'
 function App () {
 
-  const [cita, setCita] = useState({})
+  const [cita, setCita] = useState({}) // se va a utilizar para actualizar el objeto
+  const [citas, setCitas] = useState([])
+
+  useEffect(() => {
+    const citasStorage = JSON.parse(localStorage.getItem('citas')) ?? []
+    setCitas(citasStorage)
+  }, [])
 
   return (
-    <>
-      <h1 className="text-6xl font-bold text-center">Barber</h1>
-      <main className="max-w-[95%] w-[80rem] mx-auto grid lg:grid-cols-2 gap-8 my-10 ">
-        <section className='w-full'>
-          <Formulario setCita={setCita} />
+    <MagicMotion>
+      <div>
+        <h1 className="text-6xl font-bold text-center">Barber</h1>
+        <p className='text-center font-bold text-slate-700'>Citas: <span>{citas.length}</span></p>
+      </div>
+      <main className="max-w-[95%] w-[80rem] mx-auto grid lg:grid-cols-2 gap-8 my-10 relative">
+        <section className='w-full sticky'>
+          <Formulario cita={cita} setCita={setCita} citas={citas} setCitas={setCitas} />
         </section>
-        <section className="w-full">
-          Listado de citas
+        <section className="w-full flex flex-col gap-3">
+          {citas.map(c => {
+            return <Cita key={c.id} cita={c} setCita={setCita} citas={citas} setCitas={setCitas} />
+          })}
         </section>
       </main>
       <ToastContainer hideProgressBar={true} closeButton={false} autoClose={1500} />
-    </>
+    </MagicMotion>
   )
 }
 
--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,9 +1,9 @@
-const Form = ({ children, title, onSubmit }) => {
+const Form = ({ children, title, onSubmit, className }) => {
 
 
     // <Form> <p>Esto es un children</p> </Form>
     return (
-        <div className='bg-slate-50/75 backdrop-blur p-5 rounded shadow-xl'>
+        <div className={`bg-slate-50/75 backdrop-blur p-5 rounded shadow-xl ${className}`} >
             <h2 className="text-3xl font-bold">{title}</h2>
             <form
                 className='p-5 flex flex-col w-full gap-5'
